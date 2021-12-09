3ffb45c70558c78dc85a2e76ff173ece72ae88e9
Valeri Gokadze <gokadze.v1@gmail.com>
2021-12-09 19:23:36 +0400

    added media notification [BETA]

--- lib/services/audio_manager.dart
+++ lib/services/audio_manager.dart
@@ -3,9 +3,11 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:Musify/API/musify.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:audio_service/audio_service.dart';
 import '../music.dart';
 
 AudioPlayer? audioPlayer = AudioPlayer();
+AudioHandler? _audioHandler;
 
 final durationNotifier = ValueNotifier<Duration?>(Duration.zero);
 
@@ -42,14 +44,17 @@ Future<void> playSong(int id, var context) async {
 Future play() async {
   await audioPlayer?.setUrl(kUrl!);
   await audioPlayer?.play();
+  await _audioHandler?.play();
 }
 
 Future pause() async {
   await audioPlayer?.pause();
+  await _audioHandler?.pause();
 }
 
 Future stop() async {
   await audioPlayer?.stop();
+  await _audioHandler?.stop();
 }
 
 Future mute(bool muted) async {
@@ -57,5 +62,106 @@ Future mute(bool muted) async {
     audioPlayer?.setVolume(0);
   } else {
     audioPlayer?.setVolume(1);
+  }
+}
+
+Future<AudioHandler> initAudioService() async {
+  return await AudioService.init(
+    builder: () => MyAudioHandler(),
+    config: AudioServiceConfig(
+      androidNotificationChannelId: 'me.musify',
+      androidNotificationChannelName: 'Musify',
+      androidNotificationOngoing: true,
+      androidStopForegroundOnPause: true,
+    ),
+  );
+}
+
+class MyAudioHandler extends BaseAudioHandler {
+  MyAudioHandler() {
+    _notifyAudioHandlerAboutPlaybackEvents();
+  }
+
+  @override
+  Future stop() async {
+    playbackState.add(playbackState.value.copyWith(
+      processingState: AudioProcessingState.idle,
+    ));
+
+    audioPlayer?.stop();
+  }
+
+  @override
+  Future<void> play() async {
+    playbackState.add(playbackState.value.copyWith(
+      playing: true,
+      controls: [MediaControl.pause],
+    ));
+
+    mediaItem.add(MediaItem(
+      id: kUrl!,
+      album: album!,
+      title: title!,
+      artist: artist!,
+      duration: duration!,
+      artUri: Uri.parse(image!),
+    ));
+
+    audioPlayer?.play();
+  }
+
+  @override
+  Future<void> pause() async {
+    playbackState.add(playbackState.value.copyWith(
+      playing: false,
+      controls: [MediaControl.play],
+    ));
+    audioPlayer?.pause();
+  }
+
+  void _notifyAudioHandlerAboutPlaybackEvents() {
+    audioPlayer?.playbackEventStream.listen((PlaybackEvent event) {
+      final playing = audioPlayer!.playing;
+      mediaItem.add(MediaItem(
+        id: kUrl!,
+        album: album!,
+        title: title!,
+        artist: artist!,
+        duration: duration!,
+        artUri: Uri.parse(image!),
+      ));
+      playbackState.add(playbackState.value.copyWith(
+        controls: [
+          MediaControl.skipToPrevious, //not working yet
+          if (playing) MediaControl.pause else MediaControl.play,
+          MediaControl.stop,
+          MediaControl.skipToNext, // not working yet
+        ],
+        systemActions: const {
+          MediaAction.seek,
+        },
+        androidCompactActionIndices: const [0, 1, 3],
+        processingState: const {
+          ProcessingState.idle: AudioProcessingState.idle,
+          ProcessingState.loading: AudioProcessingState.loading,
+          ProcessingState.buffering: AudioProcessingState.buffering,
+          ProcessingState.ready: AudioProcessingState.ready,
+          ProcessingState.completed: AudioProcessingState.completed,
+        }[audioPlayer!.processingState]!,
+        repeatMode: const {
+          LoopMode.off: AudioServiceRepeatMode.none,
+          LoopMode.one: AudioServiceRepeatMode.one,
+          LoopMode.all: AudioServiceRepeatMode.all,
+        }[audioPlayer!.loopMode]!,
+        shuffleMode: (audioPlayer!.shuffleModeEnabled)
+            ? AudioServiceShuffleMode.all
+            : AudioServiceShuffleMode.none,
+        playing: playing,
+        updatePosition: audioPlayer!.position,
+        bufferedPosition: audioPlayer!.bufferedPosition,
+        speed: audioPlayer!.speed,
+        queueIndex: event.currentIndex,
+      ));
+    });
   }
 }
--- lib/services/locator.dart
+++ lib/services/locator.dart
@@ -1,9 +1,11 @@
+import 'package:audio_service/audio_service.dart';
 import 'package:get_it/get_it.dart';
 
 import '../music.dart';
+import 'audio_manager.dart';
 
 GetIt getIt = GetIt.instance;
 
 Future<void> setupServiceLocator() async {
-  // getIt.registerLazySingleton<AudioAppState>(() => AudioAppState());
+  getIt.registerSingleton<AudioHandler>(await initAudioService());
 }
