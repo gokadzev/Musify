c2a0659c9e70db42419ec0e7964b56ad1200c367
gokadzev <gokadze.v1@gmail.com>
2022-05-06 23:02:10 +0400

    changelog : updated dependencies changed deprecated functions

--- lib/helper/utils.dart
+++ lib/helper/utils.dart
@@ -1,8 +1,8 @@
 import 'package:url_launcher/url_launcher.dart';
 
 launchURL(url) async {
-  if (await canLaunch(url)) {
-    await launch(url);
+  if (await canLaunchUrl(url)) {
+    await launchUrl(url);
   } else {
     throw 'Could not launch $url';
   }
