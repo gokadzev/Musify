7990474dc428f46e5da85af788f8c50985ba4d8f
gokadzev <gokadze.v1@gmail.com>
2022-07-17 05:35:26 +0400

    changelog: updated workflow

--- lib/helper/version.dart
+++ lib/helper/version.dart
@@ -5,6 +5,7 @@ import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:musify/services/ext_storage.dart';
 
 late var version;
+late var dlUrl;
 const apiUrl =
     "https://raw.githubusercontent.com/gokadzev/Musify/update/check.json";
 
@@ -27,6 +28,11 @@ downloadAppUpdates() async {
   final response = await request.close();
   final contentAsString = await utf8.decodeStream(response);
   final map = json.decode(contentAsString);
+  if (await getCPUArchitecture() == "aarch64") {
+    dlUrl = map["arm64url"].toString();
+  } else {
+    dlUrl = map["url"].toString();
+  }
   final String? dlPath =
       await ExtStorageProvider.getExtStorage(dirName: 'Download');
   final File file = File("${dlPath!}/Musify.apk");
@@ -34,7 +40,7 @@ downloadAppUpdates() async {
     await file.delete();
   }
   await FlutterDownloader.enqueue(
-    url: map["url"].toString(),
+    url: dlUrl,
     savedDir: dlPath,
     saveInPublicStorage: true,
     showNotification:
@@ -42,4 +48,10 @@ downloadAppUpdates() async {
     openFileFromNotification:
         true, // click on notification to open downloaded file (for Android)
   );
+}
+
+Future<String> getCPUArchitecture() async {
+  var info = await Process.run('uname', ['-m']);
+  var cpu = info.stdout.toString().replaceAll('\n', '');
+  return cpu;
 }
