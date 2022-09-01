d9938ac7a102c2f8a872508b6b3fc96d27a3147a
gokadzev <gokadze.v1@gmail.com>
2022-09-01 10:21:37 +0400

    removed unnecessary case for lyrics

--- lib/ui/player.dart
+++ lib/ui/player.dart
@@ -506,8 +506,7 @@ class AudioAppState extends State<AudioApp> {
                                         valueListenable: lyrics,
                                         builder: (_, value, __) {
                                           if (value != 'null' &&
-                                              value != 'not found' &&
-                                              value != 'server error') {
+                                              value != 'not found') {
                                             return Expanded(
                                               child: Padding(
                                                 padding:
@@ -530,25 +529,6 @@ class AudioAppState extends State<AudioApp> {
                                           } else if (value == 'null') {
                                             return const SizedBox(
                                               child: Spinner(),
-                                            );
-                                          } else if (value == 'server error') {
-                                            return Padding(
-                                              padding: const EdgeInsets.only(
-                                                top: 120,
-                                              ),
-                                              child: Center(
-                                                child: Text(
-                                                  AppLocalizations.of(
-                                                    context,
-                                                  )!
-                                                      .serverError,
-                                                  style: TextStyle(
-                                                    color: accentLight,
-                                                    fontSize: 25,
-                                                  ),
-                                                  textAlign: TextAlign.center,
-                                                ),
-                                              ),
                                             );
                                           } else {
                                             return Padding(
