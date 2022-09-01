ae62a8a5ac35485aabdbc6579759e3b855e1bc38
gokadzev <gokadze.v1@gmail.com>
2022-09-01 09:39:47 +0400

    removed unnecessary code from player page

--- lib/ui/player.dart
+++ lib/ui/player.dart
@@ -107,45 +107,27 @@ class AudioAppState extends State<AudioApp> {
                       quality: 100,
                       artworkWidth: size.width / 1.2,
                       artworkHeight: size.height / 2.7,
-                      nullArtworkWidget: SizedBox(
+                      nullArtworkWidget: Container(
                         width: size.width / 1.2,
                         height: size.height / 2.7,
-                        child: CachedNetworkImage(
-                          imageUrl: metadata.artUri.toString(),
-                          imageBuilder: (context, imageProvider) =>
-                              DecoratedBox(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              image: DecorationImage(
-                                image: imageProvider,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: const LinearGradient(
+                            colors: [
+                              Color.fromARGB(30, 255, 255, 255),
+                              Color.fromARGB(30, 233, 233, 233),
+                            ],
                           ),
-                          placeholder: (context, url) => const Spinner(),
-                          errorWidget: (context, url, error) => Container(
-                            width: size.width / 1.2,
-                            height: size.height / 2.7,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              gradient: const LinearGradient(
-                                colors: [
-                                  Color.fromARGB(30, 255, 255, 255),
-                                  Color.fromARGB(30, 233, 233, 233),
-                                ],
-                              ),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              MdiIcons.musicNoteOutline,
+                              size: size.width / 8,
+                              color: accent,
                             ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Icon(
-                                  MdiIcons.musicNoteOutline,
-                                  size: size.width / 8,
-                                  color: accent,
-                                ),
-                              ],
-                            ),
-                          ),
+                          ],
                         ),
                       ),
                       keepOldArtwork: true,
@@ -166,9 +148,7 @@ class AudioAppState extends State<AudioApp> {
                           ),
                         ),
                         placeholder: (context, url) => const Spinner(),
-                        errorWidget: (context, url, error) => Container(
-                          width: size.width / 1.2,
-                          height: size.height / 2.7,
+                        errorWidget: (context, url, error) => DecoratedBox(
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(10),
                             gradient: const LinearGradient(
