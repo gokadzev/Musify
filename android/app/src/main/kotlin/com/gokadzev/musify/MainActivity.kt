64a511fc67c86529a01154c55a478bd97f0c48a5
gokadzev <gokadze.v1@gmail.com>
2022-09-08 10:47:18 +0400

    made changes in the splash screen (#97)

--- android/app/src/main/kotlin/com/gokadzev/musify/MainActivity.kt
+++ android/app/src/main/kotlin/com/gokadzev/musify/MainActivity.kt
@@ -1,6 +1,19 @@
-package com.gokadzev.musify
-
+import android.os.Build
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    // Aligns the Flutter view vertically with the window.
+    WindowCompat.setDecorFitsSystemWindows(getWindow(), false)
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+      // Disable the Android splash screen fade out animation to avoid
+      // a flicker before the similar frame is drawn in Flutter.
+      splashScreen.setOnExitAnimationListener { splashScreenView -> splashScreenView.remove() }
+    }
+
+    super.onCreate(savedInstanceState)
+  }
 }
