d12d015317a00bc66f4c4071fc9fb6ae65d0e5a5
gokadzev <gokadze.v1@gmail.com>
2022-10-27 20:27:06 +0400

    don't open links in the app, open with external application

--- lib/helper/url_launcher.dart
+++ lib/helper/url_launcher.dart
@@ -2,7 +2,7 @@ import 'package:url_launcher/url_launcher.dart';
 
 void launchURL(url) async {
   if (await canLaunchUrl(url)) {
-    await launchUrl(url);
+    await launchUrl(url, mode: LaunchMode.externalApplication);
   } else {
     throw 'Could not launch $url';
   }
