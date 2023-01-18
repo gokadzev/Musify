e18fd4105ed480d718bfaa7bda7a8918aafa62eb
Valeri Gokadze <gokadze.v1@gmail.com>
2023-01-18 19:52:03 +0400

    register audio service without variable

--- lib/main.dart
+++ lib/main.dart
@@ -193,17 +193,18 @@ void main() async {
 }
 
 Future<void> initialisation() async {
-  final audioHandler = await AudioService.init(
-    builder: MyAudioHandler.new,
-    config: const AudioServiceConfig(
-      androidNotificationChannelId: 'com.gokadzev.musify',
-      androidNotificationChannelName: 'Musify',
-      androidNotificationOngoing: true,
-      androidNotificationIcon: 'mipmap/launcher_icon',
-      androidShowNotificationBadge: true,
+  getIt.registerSingleton<AudioHandler>(
+    await AudioService.init(
+      builder: MyAudioHandler.new,
+      config: const AudioServiceConfig(
+        androidNotificationChannelId: 'com.gokadzev.musify',
+        androidNotificationChannelName: 'Musify',
+        androidNotificationOngoing: true,
+        androidNotificationIcon: 'mipmap/launcher_icon',
+        androidShowNotificationBadge: true,
+      ),
     ),
   );
-  getIt.registerSingleton<AudioHandler>(audioHandler);
 
   final session = await AudioSession.instance;
   await session.configure(const AudioSessionConfiguration.music());
