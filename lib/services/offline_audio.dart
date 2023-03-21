ce5ca06f1ba4588a0f2945ce125504da89d27801
Valeri Gokadze <gokadze.v1@gmail.com>
2023-03-21 19:51:58 +0400

    removed uneccessary code

--- lib/services/offline_audio.dart
+++ lib/services/offline_audio.dart
@@ -6,10 +6,6 @@ Future<List<AudioModel>> getMusic({searchQuery}) async {
   final allSongs = await _audioQuery.querySongs(
     filter: MediaFilter.forSongs(
       audioSortType: AudioSortType.TITLE,
-      orderType: OrderType.ASC_OR_SMALLER,
-      ignoreCase: true,
-      toQuery: const {},
-      toRemove: const {},
       type: const {AudioType.IS_MUSIC: true},
     ),
   );
