17128e6c2f27271ebc9cd87124979aa81f22b50a
Valeri Gokadze <79704324+gokadzev@users.noreply.github.com>
2023-03-24 20:19:50 +0400

    Create downloads.py

---
+++ downloads.py
@@ -0,0 +1,53 @@
+import requests
+import json
+import time
+
+# Define the repository you want to get the downloads for
+repo_owner = "gokadzev"
+repo_name = "Musify"
+
+# Define the URL of the Github API endpoint for releases
+api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases"
+
+# Define a dictionary to hold the total download counts for each release
+download_counts = {}
+
+# Make the initial request to the API endpoint
+response = requests.get(api_url)
+
+# Check the response status code to see if we need to handle rate limiting
+if response.status_code == 403:
+    # If we get a rate limit error, we need to wait and try again
+    reset_time = int(response.headers["X-RateLimit-Reset"])
+    sleep_time = reset_time - time.time()
+    print(f"Rate limit exceeded. Waiting {sleep_time} seconds...")
+    time.sleep(sleep_time)
+    response = requests.get(api_url)
+
+# Loop through each release and get the download count
+while "next" in response.links:
+    # Get the download count for each asset in the release
+    for release in response.json():
+        download_count = sum(
+            asset["download_count"] for asset in release["assets"]
+        )
+        download_counts[release["tag_name"]] = download_count
+
+    # Make the next request to the API endpoint
+    response = requests.get(response.links["next"]["url"])
+
+    # Check the response status code to see if we need to handle rate limiting
+    if response.status_code == 403:
+        # If we get a rate limit error, we need to wait and try again
+        reset_time = int(response.headers["X-RateLimit-Reset"])
+        sleep_time = reset_time - time.time()
+        print(f"Rate limit exceeded. Waiting {sleep_time} seconds...")
+        time.sleep(sleep_time)
+        response = requests.get(api_url)
+
+total_downloads = sum(download_counts.values())
+json_obj = {"downloads_count": total_downloads}
+
+# Write the download counts to a JSON file
+with open("downloads_count.json", "w") as f:
+    json.dump(json_obj, f)
