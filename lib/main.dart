62b3995e7ce43ba86e01a8b118aa344811b153ed
Valeri Gokadze <gokadze.v1@gmail.com>
2023-04-22 12:39:17 +0400

    default theme mode should be dark because i don't like current light mode of the app

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 
 GetIt getIt = GetIt.instance;
 bool _interrupted = false;
-ThemeMode themeMode = ThemeMode.system;
+ThemeMode themeMode = ThemeMode.dark;
 var isFdroidBuild = false;
 
 final appLanguages = <String, String>{
