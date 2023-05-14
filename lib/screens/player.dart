a751cd1781fd5b072a1098ebe9b12a99b506f22a
Valeri Gokadze <gokadze.v1@gmail.com>
2023-05-14 21:47:27 +0400

    Regenerate song details while downloading from player page to continue process correctly

--- lib/screens/player.dart
+++ lib/screens/player.dart
@@ -1,4 +1,3 @@
-import 'package:audio_service/audio_service.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
@@ -11,7 +10,6 @@ import 'package:musify/services/audio_manager.dart';
 import 'package:musify/services/download_manager.dart';
 import 'package:musify/services/settings_manager.dart';
 import 'package:musify/style/app_themes.dart';
-import 'package:musify/utilities/mediaitem.dart';
 import 'package:musify/widgets/marque.dart';
 import 'package:musify/widgets/song_bar.dart';
 import 'package:musify/widgets/spinner.dart';
@@ -285,7 +283,10 @@ class AudioAppState extends State<AudioApp> {
                                     ),
                                     onPressed: () => downloadSong(
                                       context,
-                                      mediaItemToMap(metadata as MediaItem),
+                                      getSongDetails(
+                                        0,
+                                        metadata.extras['ytid'].toString(),
+                                      ),
                                     ),
                                   ),
                                   ValueListenableBuilder<bool>(
