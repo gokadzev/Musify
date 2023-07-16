2f2accf204e25f84fcc191e4f841daf265c151cf
Valeri Gokadze <gokadze.v1@gmail.com>
2023-07-16 18:21:23 +0400

    Improve: Improved artist page.

--- lib/screens/artist_page.dart
+++ lib/screens/artist_page.dart
@@ -1,18 +1,15 @@
 import 'dart:math';
 
-import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:musify/API/musify.dart';
 import 'package:musify/extensions/l10n.dart';
 import 'package:musify/extensions/screen_size.dart';
-import 'package:musify/services/audio_manager.dart';
 import 'package:musify/style/app_themes.dart';
 import 'package:musify/utilities/flutter_toast.dart';
-import 'package:musify/utilities/formatter.dart';
 import 'package:musify/widgets/artist_cube.dart';
+import 'package:musify/widgets/local_song_bar.dart';
 import 'package:musify/widgets/song_bar.dart';
 import 'package:musify/widgets/spinner.dart';
-import 'package:on_audio_query/on_audio_query.dart' hide context;
 
 class ArtistPage extends StatefulWidget {
   const ArtistPage({super.key, required this.playlist});
@@ -37,11 +34,6 @@ class _ArtistPagePageState extends State<ArtistPage> {
     _isLoading = true;
     _hasMore = true;
     _loadMore();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
   }
 
   void _loadMore() {
@@ -89,14 +81,112 @@ class _ArtistPagePageState extends State<ArtistPage> {
             ? Column(
                 children: [
                   ArtistCube(artist: widget.playlist['title']),
-                  _buildPlaylistTitle(),
-                  _buildPlayAllButton(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12),
+                    child: Text(
+                      widget.playlist['title'].toString(),
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setActivePlaylist(widget.playlist);
+                      showToast(
+                        context,
+                        context.l10n()!.queueInitText,
+                      );
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                        colorScheme.primary,
+                      ),
+                    ),
+                    child: Text(
+                      context.l10n()!.playAll.toUpperCase(),
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ),
                   const SizedBox(height: 30),
-                  if (_songsList.isNotEmpty)
-                    _buildSongList()
-                  else
+                  if (_songsList.isNotEmpty) ...[
+                    ListView.builder(
+                      shrinkWrap: true,
+                      physics: const BouncingScrollPhysics(),
+                      itemCount:
+                          _hasMore ? _songsList.length + 1 : _songsList.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        if (index >= _songsList.length) {
+                          if (!_isLoading) {
+                            _loadMore();
+                          }
+                          return const Spinner();
+                        }
+
+                        return LocalSongBar(index, _songsList[index]);
+                      },
+                    ),
+                  ] else
                     const Spinner(),
-                  _buildOnlineSongList(),
+                  FutureBuilder(
+                    future: fetchSongsList(widget.playlist['title'].toString()),
+                    builder: (context, AsyncSnapshot<dynamic> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: Padding(
+                            padding: EdgeInsets.all(35),
+                            child: Spinner(),
+                          ),
+                        );
+                      } else if (snapshot.connectionState ==
+                          ConnectionState.done) {
+                        if (snapshot.hasError ||
+                            !snapshot.hasData ||
+                            snapshot.data.isEmpty) {
+                          return Center(
+                            child: Text(
+                              'Nothing Found!',
+                              style: TextStyle(
+                                color: colorScheme.primary,
+                                fontSize: 18,
+                              ),
+                            ),
+                          );
+                        }
+                        return Wrap(
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(
+                                top: context.screenSize.height / 55,
+                                bottom: 10,
+                                left: 20,
+                                right: 20,
+                              ),
+                              child: Text(
+                                'Online Results',
+                                style: TextStyle(
+                                  color: colorScheme.primary,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                            ListView.builder(
+                              shrinkWrap: true,
+                              addAutomaticKeepAlives: false,
+                              addRepaintBoundaries: false,
+                              physics: const BouncingScrollPhysics(),
+                              itemCount: snapshot.data.length as int,
+                              itemBuilder: (context, index) {
+                                return SongBar(snapshot.data[index], true);
+                              },
+                            )
+                          ],
+                        );
+                      } else {
+                        return const SizedBox.shrink();
+                      }
+                    },
+                  ),
                 ],
               )
             : SizedBox(
@@ -104,203 +194,6 @@ class _ArtistPagePageState extends State<ArtistPage> {
                 child: const Spinner(),
               ),
       ),
-    );
-  }
-
-  Widget _buildPlaylistTitle() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 12),
-      child: Text(
-        widget.playlist['title'].toString(),
-        textAlign: TextAlign.center,
-        style: Theme.of(context).textTheme.bodyMedium,
-      ),
-    );
-  }
-
-  Widget _buildPlayAllButton() {
-    return ElevatedButton(
-      onPressed: () {
-        setActivePlaylist(widget.playlist);
-        showToast(
-          context,
-          context.l10n()!.queueInitText,
-        );
-      },
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all<Color>(
-          colorScheme.primary,
-        ),
-      ),
-      child: Text(
-        context.l10n()!.playAll.toUpperCase(),
-        style: Theme.of(context).textTheme.bodyMedium,
-      ),
-    );
-  }
-
-  Widget _buildSongList() {
-    if (_songsList.isNotEmpty)
-      return Column(
-        children: [
-          ListView.separated(
-            shrinkWrap: true,
-            physics: const BouncingScrollPhysics(),
-            separatorBuilder: (BuildContext context, int index) =>
-                const SizedBox(height: 7),
-            itemCount: _hasMore ? _songsList.length + 1 : _songsList.length,
-            itemBuilder: (BuildContext context, int index) {
-              if (index >= _songsList.length) {
-                if (!_isLoading) {
-                  _loadMore();
-                }
-                return const Spinner();
-              }
-
-              final lsong =
-                  returnSongLayoutFromAudioModel(index, _songsList[index]);
-
-              return Container(
-                padding: const EdgeInsets.only(
-                  left: 12,
-                  right: 12,
-                  bottom: 15,
-                ),
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(20),
-                  onTap: () {
-                    playSong(lsong);
-                  },
-                  splashColor: colorScheme.primary.withOpacity(0.4),
-                  hoverColor: colorScheme.primary.withOpacity(0.4),
-                  focusColor: colorScheme.primary.withOpacity(0.4),
-                  highlightColor: colorScheme.primary.withOpacity(0.4),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      QueryArtworkWidget(
-                        id: lsong['localSongId'] as int,
-                        type: ArtworkType.AUDIO,
-                        artworkWidth: 60,
-                        artworkHeight: 60,
-                        artworkBorder: BorderRadius.circular(8),
-                        nullArtworkWidget: Container(
-                          width: 60,
-                          height: 60,
-                          decoration: BoxDecoration(
-                            color: colorScheme.primary,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: const Icon(
-                            FluentIcons.music_note_1_24_regular,
-                            size: 25,
-                            color: Colors.white,
-                          ),
-                        ),
-                        keepOldArtwork: true,
-                      ),
-                      Flexible(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Container(
-                              alignment: Alignment.centerLeft,
-                              padding: const EdgeInsets.only(
-                                left: 15,
-                              ),
-                              child: Text(
-                                overflow: TextOverflow.ellipsis,
-                                lsong['artist'].toString() == ''
-                                    ? lsong['title'].toString()
-                                    : '${lsong['artist']} - ${lsong['title']}',
-                                style: TextStyle(
-                                  color: colorScheme.primary,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          ),
-        ],
-      );
-    else
-      return SizedBox(
-        height: context.screenSize.height - 100,
-        child: const Spinner(),
-      );
-  }
-
-  Widget _buildOnlineSongList() {
-    return FutureBuilder(
-      future: fetchSongsList(widget.playlist['title'].toString()),
-      builder: (context, AsyncSnapshot<dynamic> snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.waiting:
-            return const Center(
-              child: Padding(
-                padding: EdgeInsets.all(35),
-                child: Spinner(),
-              ),
-            );
-          case ConnectionState.done:
-            if (snapshot.hasError) {
-              return const SizedBox();
-            }
-            if (!snapshot.hasData) {
-              return Center(
-                child: Text(
-                  'Nothing Found!',
-                  style: TextStyle(
-                    color: colorScheme.primary,
-                    fontSize: 18,
-                  ),
-                ),
-              );
-            }
-            return Wrap(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: context.screenSize.height / 55,
-                    bottom: 10,
-                    left: 20,
-                    right: 20,
-                  ),
-                  child: Text(
-                    'Online Results',
-                    style: TextStyle(
-                      color: colorScheme.primary,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                ),
-                ListView.separated(
-                  shrinkWrap: true,
-                  addAutomaticKeepAlives: false,
-                  addRepaintBoundaries: false,
-                  physics: const BouncingScrollPhysics(),
-                  itemCount: snapshot.data.length as int,
-                  separatorBuilder: (BuildContext context, int index) =>
-                      const SizedBox(height: 7),
-                  itemBuilder: (context, index) {
-                    return SongBar(snapshot.data[index], true);
-                  },
-                )
-              ],
-            );
-          default:
-            return const SizedBox.shrink();
-        }
-      },
     );
   }
 }
