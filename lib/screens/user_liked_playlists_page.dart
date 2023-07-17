368b40aa8a2bb9907e531bde5f0757740f8ea3f7
Valeri Gokadze <gokadze.v1@gmail.com>
2023-07-17 09:44:37 +0400

    Improve: Improved user liked playlists page.

--- lib/screens/user_liked_playlists_page.dart
+++ lib/screens/user_liked_playlists_page.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:musify/API/musify.dart';
 import 'package:musify/extensions/l10n.dart';
+import 'package:musify/style/app_themes.dart';
 import 'package:musify/widgets/playlist_cube.dart';
 
-class UserLikedPlaylistsPage extends StatefulWidget {
-  @override
-  _UserLikedPlaylistsPageState createState() => _UserLikedPlaylistsPageState();
-}
-
-class _UserLikedPlaylistsPageState extends State<UserLikedPlaylistsPage> {
+class UserLikedPlaylistsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,43 +13,51 @@ class _UserLikedPlaylistsPageState extends State<UserLikedPlaylistsPage> {
           context.l10n()!.userLikedPlaylists,
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            ValueListenableBuilder(
-              valueListenable: currentLikedPlaylistsLength,
-              builder: (_, value, __) {
-                return GridView.builder(
-                  addAutomaticKeepAlives: false,
-                  addRepaintBoundaries: false,
-                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 200,
-                    crossAxisSpacing: 20,
-                    mainAxisSpacing: 20,
+      body: ValueListenableBuilder(
+        valueListenable: currentLikedPlaylistsLength,
+        builder: (_, value, __) {
+          return userLikedPlaylists.isEmpty
+              ? Center(
+                  child: Text(
+                    context.l10n()!.noLikedPlaylists,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: colorScheme.primary,
+                    ),
                   ),
-                  shrinkWrap: true,
-                  physics: const ScrollPhysics(),
-                  itemCount: userLikedPlaylists.length,
-                  padding: const EdgeInsets.only(
-                    left: 16,
-                    right: 16,
-                    top: 16,
-                    bottom: 20,
+                )
+              : SingleChildScrollView(
+                  child: GridView.builder(
+                    addAutomaticKeepAlives: false,
+                    addRepaintBoundaries: false,
+                    gridDelegate:
+                        const SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 200,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20,
+                    ),
+                    shrinkWrap: true,
+                    physics: const ScrollPhysics(),
+                    itemCount: userLikedPlaylists.length,
+                    padding: const EdgeInsets.only(
+                      left: 16,
+                      right: 16,
+                      top: 16,
+                      bottom: 20,
+                    ),
+                    itemBuilder: (BuildContext context, index) {
+                      return Center(
+                        child: PlaylistCube(
+                          id: userLikedPlaylists[index]['ytid'],
+                          image: userLikedPlaylists[index]['image'],
+                          title: userLikedPlaylists[index]['title'].toString(),
+                        ),
+                      );
+                    },
                   ),
-                  itemBuilder: (BuildContext context, index) {
-                    return Center(
-                      child: PlaylistCube(
-                        id: userLikedPlaylists[index]['ytid'],
-                        image: userLikedPlaylists[index]['image'],
-                        title: userLikedPlaylists[index]['title'].toString(),
-                      ),
-                    );
-                  },
                 );
-              },
-            )
-          ],
-        ),
+        },
       ),
     );
   }
