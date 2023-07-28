82a93be0060190f1bc323ec9c29302a5145f3530
Valeri Gokadze <gokadze.v1@gmail.com>
2023-07-28 15:13:42 +0400

    Improve: Improved Update Manager with Added Error Handling

--- lib/services/update_manager.dart
+++ lib/services/update_manager.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:musify/API/version.dart';
 import 'package:musify/extensions/l10n.dart';
+import 'package:musify/services/logger_service.dart';
 import 'package:musify/utilities/flutter_toast.dart';
 
 late String dlUrl;
@@ -17,48 +18,62 @@ Future<void> checkAppUpdates(
   BuildContext context, {
   bool downloadUpdateAutomatically = false,
 }) async {
-  final response = await http.get(Uri.parse(apiUrl));
-  if (response.statusCode != 200) {
-    throw Exception('Failed to fetch app updates');
-  }
-  final map = json.decode(response.body) as Map<String, dynamic>;
-  if (isLatestVersionHigher(appVersion, map['version'].toString())) {
-    if (downloadUpdateAutomatically) {
-      await downloadAppUpdates();
-      showToast(
-        context,
-        '${context.l10n()!.appUpdateAvailableAndDownloading}!',
+  try {
+    final response = await http.get(Uri.parse(apiUrl));
+    if (response.statusCode != 200) {
+      Logger.log(
+        'Fetch update API call returned status code ${response.statusCode}',
       );
-    } else {
-      showToast(
-        context,
-        '${context.l10n()!.appUpdateIsAvailable}!',
-      );
+      throw Exception('Failed to fetch app updates');
     }
+    final map = json.decode(response.body) as Map<String, dynamic>;
+    if (isLatestVersionHigher(appVersion, map['version'].toString())) {
+      if (downloadUpdateAutomatically) {
+        await downloadAppUpdates();
+        showToast(
+          context,
+          '${context.l10n()!.appUpdateAvailableAndDownloading}!',
+        );
+      } else {
+        showToast(
+          context,
+          '${context.l10n()!.appUpdateIsAvailable}!',
+        );
+      }
+    }
+  } catch (e) {
+    Logger.log('Error in checkAppUpdates: $e');
   }
 }
 
 Future<void> downloadAppUpdates() async {
-  final response = await http.get(Uri.parse(apiUrl));
-  if (response.statusCode != 200) {
-    throw Exception('Failed to fetch app updates');
-  }
-  final map = json.decode(response.body) as Map<String, dynamic>;
-  final dlUrl = await getCPUArchitecture() == 'aarch64'
-      ? map['arm64url'].toString()
-      : map['url'].toString();
-  final dlPath = await FilePicker.platform.getDirectoryPath();
-  final file = File('$dlPath/Musify.apk');
-  if (await file.exists()) {
-    await file.delete();
-  }
-  final task = DownloadTask(
-    url: dlUrl,
-    filename: 'Musify.apk',
-  );
+  try {
+    final response = await http.get(Uri.parse(apiUrl));
+    if (response.statusCode != 200) {
+      Logger.log(
+        'Download update API call returned status code ${response.statusCode}',
+      );
+      throw Exception('Failed to fetch app updates');
+    }
+    final map = json.decode(response.body) as Map<String, dynamic>;
+    final dlUrl = await getCPUArchitecture() == 'aarch64'
+        ? map['arm64url'].toString()
+        : map['url'].toString();
+    final dlPath = await FilePicker.platform.getDirectoryPath();
+    final file = File('$dlPath/Musify.apk');
+    if (await file.exists()) {
+      await file.delete();
+    }
+    final task = DownloadTask(
+      url: dlUrl,
+      filename: 'Musify.apk',
+    );
 
-  await FileDownloader().download(task);
-  await FileDownloader().moveToSharedStorage(task, SharedStorage.downloads);
+    await FileDownloader().download(task);
+    await FileDownloader().moveToSharedStorage(task, SharedStorage.downloads);
+  } catch (e) {
+    Logger.log('Error in downloadAppUpdates: $e');
+  }
 }
 
 bool isLatestVersionHigher(String appVersion, String latestVersion) {
