b2b8125cb995e39e816163cb0e49f0335ed23684
Valeri Gokadze <gokadze.v1@gmail.com>
2023-08-06 20:11:43 +0400

    Improve: Add horizontal padding to the search result bar

--- lib/screens/search_page.dart
+++ lib/screens/search_page.dart
@@ -155,7 +155,10 @@ class _SearchPageState extends State<SearchPage> {
                       : _suggestionsList[index];
 
                   return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 12,
+                      horizontal: 5,
+                    ),
                     child: Card(
                       child: ListTile(
                         leading: Icon(
