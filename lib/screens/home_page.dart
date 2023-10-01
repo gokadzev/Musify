62bfb34dc266664952f7b76a7f57e11b7bc93a04
Valeri Gokadze <gokadze.v1@gmail.com>
2023-10-01 10:37:58 +0400

    Improve: Improved home page code

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -4,6 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:musify/API/musify.dart';
 import 'package:musify/extensions/l10n.dart';
 import 'package:musify/extensions/screen_size.dart';
+import 'package:musify/main.dart';
 import 'package:musify/screens/artist_page.dart';
 import 'package:musify/screens/playlists_page.dart';
 import 'package:musify/style/app_themes.dart';
@@ -43,84 +44,87 @@ class _HomePageState extends State<HomePage> {
   Widget _buildSuggestedPlaylists() {
     return FutureBuilder(
       future: getPlaylists(playlistsNum: 5),
-      builder: (context, AsyncSnapshot<List<dynamic>> data) {
-        return FutureBuilder(
-          future: getPlaylists(playlistsNum: 5),
-          builder: (context, AsyncSnapshot<List<dynamic>> data) {
-            return data.hasData
-                ? Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 16,
-                          bottom: 10,
-                          left: 20,
-                          right: 20,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            SizedBox(
-                              width: context.screenSize.width / 1.4,
-                              child: MarqueeWidget(
-                                child: Text(
-                                  context.l10n()!.suggestedPlaylists,
-                                  style: TextStyle(
-                                    color: colorScheme.primary,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w700,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            IconButton(
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => PlaylistsPage(),
-                                  ),
-                                );
-                              },
-                              icon: Icon(
-                                FluentIcons.more_horizontal_24_regular,
-                                color: colorScheme.primary,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: context.screenSize.height * 0.25,
-                        child: ListView.separated(
-                          scrollDirection: Axis.horizontal,
-                          separatorBuilder: (_, __) =>
-                              const SizedBox(width: 15),
-                          itemCount: data.data!.length,
-                          padding: const EdgeInsets.symmetric(horizontal: 15),
-                          itemBuilder: (context, index) {
-                            final playlist = data.data![index];
-                            return PlaylistCube(
-                              id: playlist['ytid'],
-                              image: playlist['image'].toString(),
-                              title: playlist['title'].toString(),
-                              size: context.screenSize.height * 0.25,
-                            );
-                          },
-                        ),
-                      ),
-                    ],
-                  )
-                : const Center(
-                    child: Padding(
-                      padding: EdgeInsets.all(35),
-                      child: Spinner(),
+      builder: _buildSuggestedPlaylistsWidget,
+    );
+  }
+
+  Widget _buildSuggestedPlaylistsWidget(
+    BuildContext context,
+    AsyncSnapshot<List<dynamic>> snapshot,
+  ) {
+    if (snapshot.connectionState == ConnectionState.waiting) {
+      return _buildLoadingWidget();
+    } else if (snapshot.hasError) {
+      logger.log(snapshot.error.toString());
+      return _buildErrorWidget(context);
+    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+      return const SizedBox.shrink();
+    }
+
+    final playlists = snapshot.data!;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(
+            top: 16,
+            bottom: 10,
+            left: 20,
+            right: 20,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                width: context.screenSize.width / 1.4,
+                child: MarqueeWidget(
+                  child: Text(
+                    context.l10n()!.suggestedPlaylists,
+                    style: TextStyle(
+                      color: colorScheme.primary,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => PlaylistsPage(),
                     ),
                   );
-          },
-        );
-      },
+                },
+                icon: Icon(
+                  FluentIcons.more_horizontal_24_regular,
+                  color: colorScheme.primary,
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: context.screenSize.height * 0.25,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            separatorBuilder: (_, __) => const SizedBox(width: 15),
+            itemCount: playlists.length,
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            itemBuilder: (context, index) {
+              final playlist = playlists[index];
+              return PlaylistCube(
+                id: playlist['ytid'],
+                image: playlist['image'].toString(),
+                title: playlist['title'].toString(),
+                size: context.screenSize.height * 0.25,
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 
@@ -128,104 +132,76 @@ class _HomePageState extends State<HomePage> {
     return FutureBuilder(
       future: getRecommendedSongs(),
       builder: (context, AsyncSnapshot<dynamic> snapshot) {
-        return FutureBuilder(
-          future: getRecommendedSongs(),
-          builder: (context, AsyncSnapshot<dynamic> snapshot) {
-            final calculatedSize = context.screenSize.height * 0.25;
-            switch (snapshot.connectionState) {
-              case ConnectionState.waiting:
-                return const Center(
-                  child: Padding(
-                    padding: EdgeInsets.all(35),
-                    child: Spinner(),
-                  ),
-                );
-              case ConnectionState.done:
-                if (snapshot.hasError) {
-                  return Center(
-                    child: Text(
-                      '${context.l10n()!.error}!',
-                      style: TextStyle(
-                        color: colorScheme.primary,
-                        fontSize: 18,
-                      ),
-                    ),
-                  );
-                }
-                if (!snapshot.hasData) {
-                  return const SizedBox.shrink();
-                }
-                return Wrap(
-                  children: <Widget>[
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            top: 16,
-                            bottom: 10,
-                            left: 20,
-                            right: 20,
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              SizedBox(
-                                width: context.screenSize.width / 1.4,
-                                child: MarqueeWidget(
-                                  child: Text(
-                                    context.l10n()!.suggestedArtists,
-                                    style: TextStyle(
-                                      color: colorScheme.primary,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w700,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(
-                          height: calculatedSize,
-                          child: ListView.separated(
-                            padding: const EdgeInsets.symmetric(horizontal: 15),
-                            scrollDirection: Axis.horizontal,
-                            separatorBuilder: (_, __) =>
-                                const SizedBox(width: 15),
-                            itemCount: 5,
-                            itemBuilder: (context, index) {
-                              final artist =
-                                  snapshot.data[index]['artist'].split('~')[0];
-                              return GestureDetector(
-                                onTap: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => ArtistPage(
-                                        playlist: {
-                                          'title': artist,
-                                        },
-                                      ),
-                                    ),
-                                  );
-                                },
-                                child: ArtistCube(artist),
-                              );
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(
-                        top: context.screenSize.height / 55,
-                        bottom: 10,
-                        left: 20,
-                        right: 20,
-                      ),
+        final calculatedSize = context.screenSize.height * 0.25;
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return _buildLoadingWidget();
+          case ConnectionState.done:
+            if (snapshot.hasError) {
+              logger.log(snapshot.error.toString());
+              return _buildErrorWidget(context);
+            }
+            if (!snapshot.hasData) {
+              return const SizedBox.shrink();
+            }
+            return _buildRecommendedContent(
+              context,
+              snapshot.data,
+              calculatedSize,
+            );
+          default:
+            return const SizedBox.shrink();
+        }
+      },
+    );
+  }
+
+  Widget _buildLoadingWidget() {
+    return const Center(
+      child: Padding(
+        padding: EdgeInsets.all(35),
+        child: Spinner(),
+      ),
+    );
+  }
+
+  Widget _buildErrorWidget(BuildContext context) {
+    return Center(
+      child: Text(
+        '${context.l10n()!.error}!',
+        style: TextStyle(
+          color: colorScheme.primary,
+          fontSize: 18,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRecommendedContent(
+    BuildContext context,
+    dynamic data,
+    double calculatedSize,
+  ) {
+    return Wrap(
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 16,
+                bottom: 10,
+                left: 20,
+                right: 20,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: context.screenSize.width / 1.4,
+                    child: MarqueeWidget(
                       child: Text(
-                        context.l10n()!.recommendedForYou,
+                        context.l10n()!.suggestedArtists,
                         style: TextStyle(
                           color: colorScheme.primary,
                           fontSize: 20,
@@ -233,26 +209,68 @@ class _HomePageState extends State<HomePage> {
                         ),
                       ),
                     ),
-                    ListView.separated(
-                      shrinkWrap: true,
-                      addAutomaticKeepAlives: false,
-                      addRepaintBoundaries: false,
-                      physics: const BouncingScrollPhysics(),
-                      itemCount: snapshot.data.length as int,
-                      separatorBuilder: (BuildContext context, int index) =>
-                          const SizedBox(height: 7),
-                      itemBuilder: (context, index) {
-                        return SongBar(snapshot.data[index], true);
-                      },
-                    ),
-                  ],
-                );
-              default:
-                return const SizedBox.shrink();
-            }
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: calculatedSize,
+              child: ListView.separated(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                scrollDirection: Axis.horizontal,
+                separatorBuilder: (_, __) => const SizedBox(width: 15),
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  final artist = data[index]['artist'].split('~')[0];
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ArtistPage(
+                            playlist: {
+                              'title': artist,
+                            },
+                          ),
+                        ),
+                      );
+                    },
+                    child: ArtistCube(artist),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+        Padding(
+          padding: EdgeInsets.only(
+            top: context.screenSize.height / 55,
+            bottom: 10,
+            left: 20,
+            right: 20,
+          ),
+          child: Text(
+            context.l10n()!.recommendedForYou,
+            style: TextStyle(
+              color: colorScheme.primary,
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+        ListView.separated(
+          shrinkWrap: true,
+          addAutomaticKeepAlives: false,
+          addRepaintBoundaries: false,
+          physics: const BouncingScrollPhysics(),
+          itemCount: data.length as int,
+          separatorBuilder: (BuildContext context, int index) =>
+              const SizedBox(height: 7),
+          itemBuilder: (context, index) {
+            return SongBar(data[index], true);
           },
-        );
-      },
+        ),
+      ],
     );
   }
 }
