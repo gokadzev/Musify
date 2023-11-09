6122a56dee11a3e00912e437258cc651fec755a5
Valeri Gokadze <gokadze.v1@gmail.com>
2023-11-09 17:35:23 +0400

    Add: Added 5 albums

--- lib/DB/albums.db.dart
+++ lib/DB/albums.db.dart
@@ -19,5 +19,33 @@ List<Map<String, dynamic>> albumsDB = [
     'image': 'https://i.scdn.co/image/ab67616d0000b2734ae1c4c5c45aabe565499163',
     'isAlbum': true,
     'list': <Map<String, dynamic>>[],
+  },
+  {
+    'ytid': 'PL9tY0BWXOZFuFZ-n1neSQ9R619SDUDSyl',
+    'title': 'Lover - Taylor Swift',
+    'image': 'https://i.scdn.co/image/ab67616d0000b273d64d9e6e9b7897ead3279cff',
+    'isAlbum': true,
+    'list': <Map<String, dynamic>>[],
+  },
+  {
+    'ytid': 'PLfSdF_HSSu55q-5p-maISZyr19erpZsTo',
+    'title': "Harry's House - Harry Styles",
+    'image': 'https://i.scdn.co/image/ab67616d0000b2732e8ed79e177ff6011076f5f0',
+    'isAlbum': true,
+    'list': <Map<String, dynamic>>[],
+  },
+  {
+    'ytid': 'PLfSdF_HSSu548MHnItRg7sYbj1OkmFc2g',
+    'title': 'Fine Line - Harry Styles',
+    'image': 'https://i.scdn.co/image/ab67616d0000b27377fdcfda6535601aff081b6a',
+    'isAlbum': true,
+    'list': <Map<String, dynamic>>[],
+  },
+  {
+    'ytid': 'PLK2zhq9oy0K4yxwMmBPsXQ3NvaQSxw6rY',
+    'title': 'My Dear Melancholy - The Weeknd',
+    'image': 'https://i.scdn.co/image/ab67616d0000b2731f6a2a40bb692936879db730',
+    'isAlbum': true,
+    'list': <Map<String, dynamic>>[],
   }
 ];
