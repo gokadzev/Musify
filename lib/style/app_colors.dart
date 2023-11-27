ba7fff4b68c61cf33095d018720f8f471e93d76d
Valeri Gokadze <gokadze.v1@gmail.com>
2023-11-27 20:37:22 +0400

    Remove: Removed 3 accent colors

--- lib/style/app_colors.dart
+++ lib/style/app_colors.dart
@@ -11,7 +11,6 @@ const availableColors = <Color>[
   Color(0xFFFFC0CB), // Pink
   Color(0xFFFFD700), // Gold
   Color(0xFF008080), // Teal
-  Color(0xFF9932CC), // Dark Orchid
   Color(0xFF00CED1), // Dark Turquoise
   Color(0xFFE9967A), // Dark Salmon
   Color(0xFFFA8072), // Salmon
@@ -19,10 +18,8 @@ const availableColors = <Color>[
   Color(0xFF5D6CC2), // Slate Blue
   Color(0xFFF8C733), // Cadmium Yellow
   Color(0xFF2196F3), // Material Blue
-  Color(0xFFF44336), // Red
   Color(0xFF4CAF50), // Green
   Color(0xFFFF9800), // Orange
-  Color(0xFFE91E63), // Material Pink
   Color(0xFF009688), // Material Teal
   Color(0xFFCDDC39), // Lime
   Color(0xFF00BCD4), // Material Cyan
