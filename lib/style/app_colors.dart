ca52f794cb4f3cc949ce8951f030eaee2b0cdbc2
Valeri Gokadze <gokadze.v1@gmail.com>
2023-11-27 20:55:52 +0400

    Improve: Improved colors (sort & removes)

--- lib/style/app_colors.dart
+++ lib/style/app_colors.dart
@@ -1,29 +1,24 @@
 import 'package:flutter/material.dart';
 
 const availableColors = <Color>[
-  Color(0xFF9ACD32), // Yellow Green
-  Color(0xFF00FA9A), // Medium Spring Green
-  Color(0xFFF08080), // Light Coral
-  Color(0xFF6495ED), // Cornflower Blue
-  Color(0xFFC8B6FF), // Lavender Blue
-  Color(0xFFE2C09F), // Pale Peach
-  Color(0xFFAABBCC), // Light Steel Blue
-  Color(0xFFFFC0CB), // Pink
-  Color(0xFFFFD700), // Gold
-  Color(0xFF008080), // Teal
-  Color(0xFF00CED1), // Dark Turquoise
-  Color(0xFFE9967A), // Dark Salmon
-  Color(0xFFFA8072), // Salmon
-  Color(0xFFBDB76B), // Dark Khaki
-  Color(0xFF5D6CC2), // Slate Blue
-  Color(0xFFF8C733), // Cadmium Yellow
-  Color(0xFF2196F3), // Material Blue
-  Color(0xFF4CAF50), // Green
-  Color(0xFFFF9800), // Orange
   Color(0xFF009688), // Material Teal
-  Color(0xFFCDDC39), // Lime
   Color(0xFF00BCD4), // Material Cyan
+  Color(0xFF00FA9A), // Medium Spring Green
+  Color(0xFF4CAF50), // Green
+  Color(0xFF9ACD32), // Yellow Green
+  Color(0xFFCDDC39), // Lime
+  Color(0xFFF08080), // Light Coral
+  Color(0xFFE9967A), // Dark Salmon
+  Color(0xFFFFC0CB), // Pink
+  Color(0xFF2196F3), // Material Blue
+  Color(0xFF6495ED), // Cornflower Blue
+  Color(0xFFA6C8FF), // Lighter Cornflower Blue
+  Color(0xFFFF9800), // Orange
+  Color(0xFFF8C733), // Cadmium Yellow
+  Color(0xFFFFD700), // Gold
+  Color(0xFFBDB76B), // Dark Khaki
   Color(0xFFC4A092), // Material Brown
-  Color(0xFF9E9E9E), // Material Grey
+  Color(0xFFE2C09F), // Pale Peach
   Color(0xff91cef4), // Material Skycolor
+  Color(0xFFAABBCC), // Light Steel Blue
 ];
