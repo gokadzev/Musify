f784b26775b36ebc8259afaea724ff539eadbf43
Valeri Gokadze <gokadze.v1@gmail.com>
2023-11-29 12:06:52 +0400

    Improve: Removed unused variable

--- lib/utilities/mediaitem.dart
+++ lib/utilities/mediaitem.dart
@@ -1,9 +1,6 @@
 import 'package:audio_service/audio_service.dart';
 import 'package:just_audio/just_audio.dart';
 
-const noImageVar =
-    'https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bXVzaWN8ZW58MHx8MHx8&w=500&q=80';
-
 Map mediaItemToMap(MediaItem mediaItem) => {
       'id': mediaItem.id,
       'ytid': mediaItem.extras!['ytid'],
