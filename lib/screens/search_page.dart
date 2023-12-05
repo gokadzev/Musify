a4182ffeb91731ec4e8ef9ff4e8f11eba747d3df
Valeri Gokadze <gokadze.v1@gmail.com>
2023-12-05 21:54:31 +0400

    Improve: Improved search history items spacing

--- lib/screens/search_page.dart
+++ lib/screens/search_page.dart
@@ -106,10 +106,7 @@ class _SearchPageState extends State<SearchPage> {
                       : _suggestionsList[index];
 
                   return Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 12,
-                      horizontal: 5,
-                    ),
+                    padding: const EdgeInsets.all(5),
                     child: Card(
                       child: ListTile(
                         leading: Icon(
