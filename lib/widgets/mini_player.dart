b2b9c415ac4389bbba0d5d110be0c271ca1df47e
Valeri Gokadze <gokadze.v1@gmail.com>
2023-12-17 14:50:39 +0400

    Improve: Improved mini player (swipe up to expand)

--- lib/widgets/mini_player.dart
+++ lib/widgets/mini_player.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:musify/main.dart';
 import 'package:musify/screens/now_playing_page.dart';
 import 'package:musify/style/app_themes.dart';
+import 'package:musify/widgets/marque.dart';
 import 'package:musify/widgets/no_artwork_cube.dart';
 
 class MiniPlayer extends StatelessWidget {
@@ -13,51 +14,40 @@ class MiniPlayer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 75,
-      decoration: BoxDecoration(
-        color: colorScheme.onSecondary,
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(18),
-          topRight: Radius.circular(18),
+    return GestureDetector(
+      onVerticalDragUpdate: (details) {
+        if (details.primaryDelta! < 0) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const NowPlayingPage(),
+            ),
+          );
+        }
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        height: 75,
+        decoration: BoxDecoration(
+          color: colorScheme.onSecondary,
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(18),
+            topRight: Radius.circular(18),
+          ),
+        ),
+        child: Row(
+          children: <Widget>[
+            _buildArtwork(),
+            _buildMetadata(),
+            StreamBuilder<PlaybackState>(
+              stream: audioHandler.playbackState,
+              builder: (context, snapshot) {
+                return _buildPlaybackIconButton(snapshot.data);
+              },
+            ),
+          ],
         ),
       ),
-      child: Row(
-        children: <Widget>[
-          _buildExpandButton(context),
-          _buildArtwork(),
-          _buildMetadata(),
-          const Spacer(),
-          StreamBuilder<PlaybackState>(
-            stream: audioHandler.playbackState,
-            builder: (context, snapshot) {
-              return Padding(
-                padding: const EdgeInsets.only(right: 8),
-                child: _buildPlaybackIconButton(snapshot.data),
-              );
-            },
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildExpandButton(BuildContext context) {
-    return IconButton(
-      padding: const EdgeInsets.symmetric(horizontal: 15),
-      icon: Icon(
-        FluentIcons.arrow_up_24_filled,
-        size: 22,
-        color: Theme.of(context).colorScheme.primary,
-      ),
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const NowPlayingPage(),
-          ),
-        );
-      },
     );
   }
 
@@ -80,33 +70,36 @@ class MiniPlayer extends StatelessWidget {
   }
 
   Widget _buildMetadata() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          _truncateText(metadata.title, 15),
-          style: TextStyle(
-            color: colorScheme.primary,
-            fontSize: 17,
-            fontWeight: FontWeight.w600,
-          ),
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(right: 8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            MarqueeWidget(
+              child: Text(
+                metadata.title,
+                style: TextStyle(
+                  color: colorScheme.primary,
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            MarqueeWidget(
+              child: Text(
+                metadata.artist.toString(),
+                style: TextStyle(
+                  color: colorScheme.primary,
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          ],
         ),
-        Text(
-          _truncateText(metadata.artist.toString(), 15),
-          style: TextStyle(
-            color: colorScheme.primary,
-            fontSize: 15,
-          ),
-        ),
-      ],
+      ),
     );
-  }
-
-  String _truncateText(String text, int maxLength) {
-    return text.length > maxLength
-        ? '${text.substring(0, maxLength)}...'
-        : text;
   }
 }
 
@@ -132,10 +125,13 @@ Widget _buildPlaybackIconButton(PlaybackState? playerState) {
     onPressed = () => audioHandler.seek(Duration.zero);
   }
 
-  return IconButton(
-    icon: Icon(icon, color: colorScheme.primary),
-    iconSize: 45,
-    onPressed: onPressed,
+  return InkWell(
+    onTap: onPressed,
     splashColor: Colors.transparent,
+    child: Icon(
+      icon,
+      color: colorScheme.primary,
+      size: 40,
+    ),
   );
 }
