gokadzev/Musify

commit a69a28c36f3088ba351922971f12ebc49653c899

Improved code

lib/main.dart
@@ -283,11 +283,11 @@ void main() async {
 
 Future<void> initialisation() async {
   try {
-    await Hive.initFlutter();
-    await Hive.openBox('settings');
-    await Hive.openBox('user');
-    await Hive.openBox('userNoBackup');
-    await Hive.openBox('cache');
+    final boxNames = ['settings', 'user', 'userNoBackup', 'cache'];
+
+    for (final boxName in boxNames) {
+      await Hive.openBox(boxName);
+    }
 
     audioHandler = await AudioService.init(
       builder: MusifyAudioHandler.new,

commit 24753a090c6d11a901d0babf15df12c57f80cd1f

Fix: Fixed the error caused by the commit a69a28c

lib/main.dart
@@ -283,6 +283,8 @@ void main() async {
 
 Future<void> initialisation() async {
   try {
+    await Hive.initFlutter();
+
     final boxNames = ['settings', 'user', 'userNoBackup', 'cache'];
 
     for (final boxName in boxNames) {