e34dc45bfa344f34d2a0fcfe7b5770cee4edaa2b
Valeri Gokadze <gokadze.v1@gmail.com>
2024-04-30 21:42:46 +0400

    Improve: Removed unnecessary routes

--- lib/services/router_service.dart
+++ lib/services/router_service.dart
@@ -192,20 +192,6 @@ class NavigationManager {
             },
             routes: [
               GoRoute(
-                path: 'userSongs/:page',
-                builder: (context, state) => UserSongsPage(
-                  page: state.pathParameters['page'] ?? 'liked',
-                ),
-              ),
-              GoRoute(
-                path: 'playlists',
-                builder: (context, state) => const PlaylistsPage(),
-              ),
-              GoRoute(
-                path: 'userLikedPlaylists',
-                builder: (context, state) => const UserLikedPlaylistsPage(),
-              ),
-              GoRoute(
                 path: 'license',
                 builder: (context, state) => const LicensePage(
                   applicationName: 'Musify',
