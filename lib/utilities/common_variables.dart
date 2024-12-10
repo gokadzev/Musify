b149145e812c5189c62a17f09186137f2e2462a4
Valeri Gokadze <gokadze.v1@gmail.com>
2024-12-10 10:51:01 +0400

    style(ui): update common padding value for bars

--- lib/utilities/common_variables.dart
+++ lib/utilities/common_variables.dart
@@ -21,7 +21,7 @@
 
 import 'package:flutter/material.dart';
 
-const commonBarPadding = EdgeInsets.symmetric(horizontal: 10, vertical: 4);
+const commonBarPadding = EdgeInsets.symmetric(horizontal: 8, vertical: 1);
 var commonBarRadius = BorderRadius.circular(12);
 var commonBarTitleStyle = const TextStyle(
   fontSize: 15,
