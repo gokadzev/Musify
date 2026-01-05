ee51adbdcb9e9a21c0d14f7b2f8d28dc69576482
Valeri Gokadze <gokadze.v1@gmail.com>
2026-01-05 09:57:22 +0400

    refactor: move artwork and playback control widgets from now playing page into separate files

---
+++ lib/widgets/now_playing/marquee_text_widget.dart
@@ -0,0 +1,53 @@
+/*
+ *     Copyright (C) 2026 Valeri Gokadze
+ *
+ *     Musify is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Musify is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ *     For more information about Musify, including how to contribute,
+ *     please visit: https://github.com/gokadzev/Musify
+ */
+
+import 'package:flutter/material.dart';
+import 'package:musify/widgets/marque.dart';
+
+class MarqueeTextWidget extends StatelessWidget {
+  const MarqueeTextWidget({
+    super.key,
+    required this.text,
+    required this.fontColor,
+    required this.fontSize,
+    required this.fontWeight,
+  });
+  final String text;
+  final Color fontColor;
+  final double fontSize;
+  final FontWeight fontWeight;
+
+  @override
+  Widget build(BuildContext context) {
+    return MarqueeWidget(
+      backDuration: const Duration(seconds: 1),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: fontSize,
+          fontWeight: fontWeight,
+          color: fontColor,
+          letterSpacing: 0.2,
+        ),
+      ),
+    );
+  }
+}
